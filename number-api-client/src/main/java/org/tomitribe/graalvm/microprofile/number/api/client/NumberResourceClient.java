6b33b7d1d5bc9d9dd1ecc214def54cdbca3bcc6c
Roberto Cortez <radcortez@yahoo.com>
2018-10-16 18:45:28 +0100

    Fixed issues with CDI.

--- number-api-client/src/main/java/org/tomitribe/graalvm/microprofile/number/api/client/NumberResourceClient.java
+++ number-api-client/src/main/java/org/tomitribe/graalvm/microprofile/number/api/client/NumberResourceClient.java
@@ -2,14 +2,18 @@ package org.tomitribe.graalvm.microprofile.number.api.client;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import javax.enterprise.context.Dependent;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+@Dependent
 @RegisterRestClient
 @Path("/number")
 @Produces(MediaType.TEXT_PLAIN)
+@Consumes(MediaType.TEXT_PLAIN)
 public interface NumberResourceClient {
     @GET
     @Path("/generate")

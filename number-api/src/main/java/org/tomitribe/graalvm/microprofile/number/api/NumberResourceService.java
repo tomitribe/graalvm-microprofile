906aced93572dfef8acd903c27dac0ef130e7b8f
Roberto Cortez <radcortez@yahoo.com>
2019-09-09 17:35:09 +0100

    Added configuration for native-image reflection lookup.

--- number-api/src/main/java/org/tomitribe/graalvm/microprofile/number/api/NumberResourceService.java
+++ number-api/src/main/java/org/tomitribe/graalvm/microprofile/number/api/NumberResourceService.java
@@ -6,7 +6,7 @@ import static spark.Spark.get;
 import static spark.Spark.port;
 
 public class NumberResourceService {
-    public static void main(String[] args) throws Exception {
+    public static void main(final String[] args) {
         final Integer port = ConfigProvider.getConfig().getOptionalValue("NUMBER_API_PORT", Integer.class).orElse(3001);
         port(port);
         System.out.println("Server running on port " + port);
